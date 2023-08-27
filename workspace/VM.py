96d3174bec02f72f68f1a4116219d5a6a1970942
Courage Ekwonye <122633331+couragekwonye@users.noreply.github.com>
2023-08-26 23:04:49 -0500

    Add files via upload

---
+++ workspace/VM.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+# Read the CSV file
+csv_file_path = r'c:\Users\Owner\Downloads\CLEAN_FleetMaintenanceRecords.csv'
+df = pd.read_csv(csv_file_path)
+
+# Dictionary mapping state abbreviations to full names
+us_state_abbreviations = {
+    'AL': 'Alabama',
+    'AK': 'Alaska',
+    'AZ': 'Arizona',
+    'AR': 'Arkansas',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'HI': 'Hawaii',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'IA': 'Iowa',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'ME': 'Maine',
+    'MD': 'Maryland',
+    'MA': 'Massachusetts',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MS': 'Mississippi',
+    'MO': 'Missouri',
+    'MT': 'Montana',
+    'NE': 'Nebraska',
+    'NV': 'Nevada',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NY': 'New York',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VT': 'Vermont',
+    'VA': 'Virginia',
+    'WA': 'Washington',
+    'WV': 'West Virginia',
+    'WI': 'Wisconsin',
+    'WY': 'Wyoming',
+    'DC': 'District of Columbia'
+}
+
+# Map state abbreviations to full names and create a new 'Full State Name' column
+df['State'] = df['State'].map(us_state_abbreviations)
+
+# Save the modified DataFrame back to the CSV file
+output_csv_path = r'c:\Users\Owner\Downloads\CLEAN_FleetMaintenanceRecords_modified.csv'
+df.to_csv(output_csv_path, index=False)
+
+print("Modified data saved in a new CSV file.")
